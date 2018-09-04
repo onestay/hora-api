ac85cb756ff228385fd45a3a2c60c281ea747104
onestay <onestay@onestay.moe>
2018-09-04 20:22:51 +0200

    better error checking

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,9 +1,10 @@
 const errors = require('restify-errors');
+const sequelize = require('sequelize');
 
 module.exports = async (req, res, next, db) => {
 	try {
 		await db.models.user.create({
-			id: 125,
+			id: 12234,
 			displayName: req.body.name,
 			email: req.body.email,
 			password: req.body.password,
@@ -14,7 +15,9 @@ module.exports = async (req, res, next, db) => {
 			email: req.body.email,
 		});
 	} catch (e) {
-		console.log(e);
+		if (e instanceof sequelize.ValidationError) {
+			return next(new errors.InvalidContentError(e.errors.map(error => error.message).join(', ')));
+		}
 		return next(new errors.InternalServerError('Something went wrong. Please try again later.'));
 	}
 	return next();

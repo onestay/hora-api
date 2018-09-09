f288971d6b4ed64286ef4dbb29ccfa265cabf409
onestay <onestay@onestay.moe>
2018-09-09 11:37:04 +0200

    update event query so only some user info is returned

--- src/routes/events/get.js
+++ src/routes/events/get.js
@@ -1,22 +1,26 @@
 const errors = require('restify-errors');
 const sequelize = require('sequelize');
+const config = require('../../config');
 
 module.exports = async (req, res, next, db) => {
 	try {
 		const event = await db.models.event.findOne({
 			where: { id: req.query.id },
-			include: [db.models.user],
+			attributes: { exclude: ['owner'] }, // we don't really need the owner since we give back the user object
+			include: [{
+				model: db.models.user,
+				attributes: ['id', 'displayName', 'name'],
+			}],
 		});
 		if (!event) {
 			return next(new errors.NotFoundError('No event with provided ID found'));
 		}
-		// TODO: omit sensetive values, like password from user
 		res.send(event);
 	} catch (e) {
 		if (e instanceof sequelize.ValidationError) {
 			return next(new errors.InvalidContentError(e.errors.map(error => error.message).join(', ')));
 		}
-		console.log(e);
+		config.log.error(`Error in event get function: ${e}`);
 		return next(new errors.InternalServerError('Something went wrong. Please try again later.'));
 	}
 

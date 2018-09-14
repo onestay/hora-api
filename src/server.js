6fa0b66448cb181c5c83b7d75e3a4b9f00a7ce93
onestay <onestay@onestay.moe>
2018-09-14 18:20:40 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const restify = require('restify');
+const errors = require('restify-errors');
 
 class Server {
 	constructor(config) {
@@ -9,7 +10,12 @@ class Server {
 		this.server.use(restify.plugins.bodyParser());
 		this.server.use(restify.plugins.queryParser());
 		this.server.use(restify.plugins.acceptParser(this.server.acceptable));
-
+		this.server.use((req, res, next) => {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+			next();
+		});
+		this.server.on('MethodNotAllowed', this.unknownMethod);
 		this.config = config;
 		this.routes = {};
 	}
@@ -18,6 +24,16 @@ class Server {
 		this.server.listen(this.config.port || 3000, () => {
 			this.config.log.info(`${this.server.name} started on ${this.server.url}`);
 		});
+	}
+
+	unknownMethod(req, res) {
+		if (req.method.toLowerCase() === 'options') {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+			res.send(200);
+		} else {
+			res.send(new errors.MethodNotAllowedError(`${req.method} is not allowed`));
+		}
 	}
 }
 
